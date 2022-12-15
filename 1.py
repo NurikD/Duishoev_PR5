cbabca06983328612d046bd842eb365ba0c2014d
Nurik <114292339+NurikD@users.noreply.github.com>
2022-12-16 00:16:35 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+a = float(input())
+yard = a/91.44*100
+print(int(a), "метров равно", yard, "ярд")
+
+
+
---
+++ 12.py
@@ -0,0 +1,9 @@
+import math
+d = float(input())
+a = d/math.sqrt(2)
+p = a*4
+s = a**2
+print("сторона квадрата равна:",a, "периметр равен:",p, "площадь равна:",s, sep = "\n" )
+
+
+
